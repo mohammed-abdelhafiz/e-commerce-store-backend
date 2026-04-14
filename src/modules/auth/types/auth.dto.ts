e100183d5385d4357d3e29d975f7eca32b93a1ff
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-04-14 12:38:44 +0200

    feat: update Zod validation schemas for user registration

--- src/modules/auth/types/auth.dto.ts
+++ src/modules/auth/types/auth.dto.ts
@@ -6,7 +6,16 @@ export const registerSchema = z.object({
     .min(3, "Name must be at least 3 characters long")
     .max(30, "Name must be at most 30 characters long"),
   email: z.email("Invalid email"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(
+      /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+      "Password must contain at least one special character"
+    ),
 });
 
 export const loginSchema = z.object({
